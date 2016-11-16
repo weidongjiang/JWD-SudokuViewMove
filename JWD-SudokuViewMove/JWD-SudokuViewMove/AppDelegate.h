a3861536601b7023fdad466ef7c9b6cd37809061
蒋伟东 <jiangweidong@yixia.com>
2016-11-16 13:07:59 +0800

    创建项目

---
+++ JWD-SudokuViewMove/JWD-SudokuViewMove/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWD-SudokuViewMove
+//
+//  Created by 蒋伟东 on 2016/11/16.
+//  Copyright © 2016年 YIXIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JWD-SudokuViewMove/JWD-SudokuViewMove/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JWD-SudokuViewMove
+//
+//  Created by 蒋伟东 on 2016/11/16.
+//  Copyright © 2016年 YIXIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
